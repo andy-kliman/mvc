4434d80800a72824af9e33af77a7a9c9f0c7c459
Andy <anry025@gmail.com>
2013-12-27 21:30:22 +0400

    Добавлен класс Model

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	// Испольовать autoloader
 	require 'libs/Bootstrap.php';
 	require 'libs/Controller.php';
+	require 'libs/Model.php';
 	require 'libs/View.php';
 	
 	$app = new Bootstrap();
---
+++ libs/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+	class Model{
+
+		function __construct(){
+
+			//$this->database = new Database();
+
+		}
+	}
+?>
